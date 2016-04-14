5863c4b95439adab3fddc4de4c3b4c67725f9ef8
Stefan Nelson <pulsestaysconstant@gmail.com>
2016-04-14 09:28:18 -0500

    Some grammar fixes in the documentation.

--- glowing_waffles/coordinates.py
+++ glowing_waffles/coordinates.py
@@ -14,28 +14,28 @@ def convert_pixel_wcs(ccd_image, lon_or_ra, lat_or_dec, is_pix=True, origin=0):
     ccd_image: CCDData object
         Image which has the coordinates to convert.
 
-    lon_or_ra: numpy.ndarray (or float)
+    lon_or_ra: `numpy.ndarray' (or float)
         An array of coordinates (or a single coordinate).
         Represents the lon/ra axis, depending on what is
         passed in.
 
-    lat_or_dec: numpy.ndarray (or float)
+    lat_or_dec: `numpy.ndarray' (or float)
         An array of coordinates (or a single coordinate).
         Represents the lat/dec axis, depending on what is
         passed in.
 
-    is_pix: Bool
+    is_pix: bool
         Used to determine what coordinates are being inputted.
         Assumes pixel coordinates by default.
 
-    origin: Int
+    origin: int
         The coordinate in the upper left corner of the image.
         Generally, this is 1 in FITS and Fortran standards,
         or 0 in Numpy and C standards. Defaults to 0.
 
     Returns
     -------
-    Returns two numpy.ndarray objects, the first being lon/RA,
+    Returns two `numpy.ndarray' objects, the first being lon/RA,
     and the second lat/Dec.
 
     """
