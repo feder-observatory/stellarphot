38f0d494d32ee03184080e1101330a354654e3a9
me <me@localhost.localdomain>
2016-04-15 13:59:17 -0500

    Added flux error column to return table

--- glowing_waffles/photometry.py
+++ glowing_waffles/photometry.py
@@ -10,7 +10,7 @@ from .coordinates import convert_pixel_wcs
 __all__ = ['photutils_stellar_photometry']
 
 def photutils_stellar_photometry(ccd_image, sources, aperture_radius, inner_annulus
-                                 outer_annulus):
+                                 outer_annulus, gain):
     """
     Perform aperture photometry on an image, with a few options for estimating
     the local background from an annulus around the aperture.
@@ -32,6 +32,15 @@ def photutils_stellar_photometry(ccd_image, sources, aperture_radius, inner_annu
 
     outer_annulus : float
         Outer radius of the annulus in pixels.
+
+    gain : float
+        Gain of the CCD. In units of electrons per DN.
+
+    N_R : float
+        Read noise of the CCD in electrons per pixel.
+
+    N_dark_pp : float
+        Number of dark counts per pixel.
 
     Returns
     -------
@@ -75,14 +84,9 @@ def photutils_stellar_photometry(ccd_image, sources, aperture_radius, inner_annu
     phot_table['RA_center'] = ra
     phot_table['Dec_center'] = dec
 
+    # Obtain flux error and add column to return table
+    noise = np.sqrt(gain*net_flux + n_pix_ap*(1 + (n_pix_ap/n_pix_ann))
+			 *(gain*(bkgd_pp + N_dark_pp) + (N_R**2)))
+    phot_table['aperture_sum_err'] = np.ones(len(phot_table['aperture_sum'])) * noise
 
-    # Return a table that includes at least:
-    #   + ra, dec of center
-    #   + x, y of center
-    #   + flux
-    #   + sky background per pixel
-    #   + aperture radius used
-    #   + annulus radii used
-    #   + net flux
-    #   + flux error
     return phot_table
