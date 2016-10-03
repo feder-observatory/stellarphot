12317ee387e6127298765f9a210f2cc7037f28b7
Matthew Craig <mattwcraig@gmail.com>
2016-10-03 13:45:58 -0500

    Add first (broken) draft of function for determining magnitude transforms

--- glowing_waffles/differential_photometry/filter_transform_ivezic_2007.py
+++ glowing_waffles/differential_photometry/filter_transform_ivezic_2007.py
@@ -1,17 +1,14 @@
-
-# coding: utf-8
-
-# #Basic filter transforms from Ivezic et all (2007)
-
-# In[4]:
-
 from __future__ import print_function, division
 
+import numpy as np
+from scipy import optimize
+
+import astropy.units as u
 from astropy.table import Table
+from astropy.coordinates import SkyCoord
 
 __all__ = ['filter_transform']
 
-# In[7]:
 
 def filter_transform(mag_data, output_filter, R=None, B=None,
                      V=None, I=None, g=None, r=None, i=None):
@@ -131,7 +128,95 @@ def filter_transform(mag_data, output_filter, R=None, B=None,
         raise ValueError('the desired filter must be a string R B V or I')
 
 
-# In[ ]:
+def standard_magnitude_transform(instrumental,
+                                 catalog,
+                                 match_radius=0.5 * u.arcsec,
+                                 color_from='catalog',
+                                 color_1='B',
+                                 color_2='V',
+                                 for_filter=None):
+    """
+    Given catalog magnitudes and instrumental magnitudes, calculate transform
+    from instrumental to catalog. Colors can be drawn from the catalog or
+    from instrumental magnitudes.
+
+    Parameters
+    ----------
+
+    instrumental: astropy Table
+        Table containing instrumental magnitudes from which transform
+        coefficients are to be determined. Each filter should be a column
+        whose name is the filter. In addition there should be a column
+        called 'RA' and a column called 'Dec'.
+    catalog : astropy Table
+        Table containing catalog magnitudes from which transform
+        coefficients are to be determined. Each filter should be a column
+        whose name is the filter. In addition there should be a column
+        called 'RA' and a column called 'Dec'.
+    match_radius : astropy Quantity  or float
+        Determines cutoff for matching coordinates of instrumental
+        magnitudes to catalog magnitudes. An on-sky separation of
+        less than match_radius counts as a match. If no units are provided
+        it is assumed the unit is degrees.
+    """
+    i_filters = [name for name in instrumental.colnames
+                 if name not in ['RA', 'Dec']]
+
+    c_filters = [name for name in catalog.colnames
+                 if name not in ['RA', 'Dec']]
+
+    if not set(i_filters).issubset(set(c_filters)):
+        raise ValueError('Some filters in instrumental table not found in '
+                         'catalog table.')
+    instrumental_coords = SkyCoord(ra=instrumental['RA'],
+                                   dec=instrumental['Dec'])
+    catalog_coords = SkyCoord(ra=catalog['RA'], dec=catalog['Dec'])
+
+    catalog_index, d2d, d3d = \
+        catalog_coords.match_to_catalog_sky(instrumental_coords)
+
+    good_match = d2d < match_radius
+
+    if not good_match.sum():
+        raise ValueError('No matches found between instrumental'
+                         ' and catalog tables.')
 
 
+    # Create empty list for the corrections (slope and intercept)
+    corrections = []
+    # Create empy list for the error in the corrections
+    # loop over all images
+    for idx in range(aij_mags.shape[1]):
+        # create BminusV list
+        BminusV = []
+        # Create Rminusr list
+        Rminusr = []
+        # loop over the apass_index and the placement in the apass_index
+        for aij_star, el in enumerate(apass_index):
+            # check if the aij index corresponds to 18 or 23 (I don't know why)
+            if aij_star in [18, 23]:
+                # Breakes out of the apass_index loop?
+                continue
+            # check if the aij star has a corresponding apass match
+            if good_match[aij_star]:
+                # Make sure the aij stars magnitude in the image isn't friggin
+                # huge
+                if aij_stars[aij_star].magnitude[idx] < 100:
+                    # Add the color of that star (according to apass) to the
+                    # bminusv list
+                    BminusV.append(apass_color[el])
+                    # add the difference between aijs magnitude and apass
+                    # transformed magnitude to the Rminusr list
+                    Rminusr.append(
+                        apass_R_mags[el] - aij_stars[aij_star].magnitude[idx])
+        # findes the slope and the slope intercept (and error in) for a plot of Rminusr vs BminusV for that image
+        # non huber way of getting slope... slope_intercept, cov =
+        # np.polyfit(BminusV, Rminusr, 1, cov=True)
+        dy = np.zeros(len(BminusV)) + 0.01
+        f_huber = lambda beta: huber_loss(beta[0], beta[1], x=np.array(
+            BminusV), y=np.array(Rminusr), dy=dy, c=1)
+        beta0 = (0.1, 20)
+        beta_huber = optimize.fmin(f_huber, beta0)
+        # append corrections data to list
+        corrections.append(beta_huber)
 
