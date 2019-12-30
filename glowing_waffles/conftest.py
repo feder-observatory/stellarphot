239ca2e6f1767c4e8a34a7b97071e664b0a79ea6
Matt Craig <mattwcraig@gmail.com>
2019-12-30 17:05:02 -0600

    Clean up conftest a bit

--- glowing_waffles/conftest.py
+++ glowing_waffles/conftest.py
@@ -2,39 +2,20 @@
 # test infrastructure.
 import os
 
-try:
-    # When the pytest_astropy_header package is installed
-    from pytest_astropy_header.display import (PYTEST_HEADER_MODULES,
-                                               TESTED_VERSIONS)
-
-    def pytest_configure(config):
-        config.option.astropy_header = True
-except ImportError:
-    # TODO: Remove this when astropy 2.x and 3.x support is dropped.
-    # Probably an old pytest-astropy package where the pytest_astropy_header
-    # is not a dependency.
-    try:
-        from astropy.tests.plugins.display import (pytest_report_header,
-                                                   PYTEST_HEADER_MODULES,
-                                                   TESTED_VERSIONS)
-    except ImportError:
-        # TODO: Remove this when astropy 2.x support is dropped.
-        # If that also did not work we're probably using astropy 2.0
-        from astropy.tests.pytest_plugins import (pytest_report_header,
-                                                  PYTEST_HEADER_MODULES,
-                                                  TESTED_VERSIONS)
-
-try:
-    # TODO: Remove this when astropy 2.x support is dropped.
-    # This is the way to get plugins in astropy 2.x
-    from astropy.tests.pytest_plugins import *
-except ImportError:
-    # Otherwise they are installed as separate packages that pytest
-    # automagically finds.
-    pass
+from pytest_astropy_header.display import PYTEST_HEADER_MODULES, TESTED_VERSIONS
 
 
-from astropy.tests.helper import enable_deprecations_as_exceptions
+def pytest_configure(config):
+
+    config.option.astropy_header = True
+    PYTEST_HEADER_MODULES.pop('h5py', None)
+
+    from .version import version
+    packagename = os.path.basename(os.path.dirname(__file__))
+    TESTED_VERSIONS[packagename] = version
+
+
+# from astropy.tests.helper import enable_deprecations_as_exceptions
 
 ## Uncomment the following line to treat all DeprecationWarnings as
 ## exceptions. For Astropy v2.0 or later, there are 2 additional keywords,
@@ -47,21 +28,3 @@ from astropy.tests.helper import enable_deprecations_as_exceptions
 ## MAJOR.MINOR or later, add:
 ##     warnings_to_ignore_by_pyver={(MAJOR, MINOR): ['Message to ignore']}
 # enable_deprecations_as_exceptions()
-
-# Customize the following lines to add/remove entries from
-# the list of packages for which version numbers are displayed when running
-# the tests. Making it pass for KeyError is essential in some cases when
-# the package uses other astropy affiliated packages.
-try:
-    PYTEST_HEADER_MODULES['Astropy'] = 'astropy'
-    del PYTEST_HEADER_MODULES['h5py']
-    del PYTEST_HEADER_MODULES['astropy-helpers']
-except KeyError:
-    pass
-
-# This is to figure out the package version, rather than
-# using Astropy's
-from .version import version
-
-packagename = os.path.basename(os.path.dirname(__file__))
-TESTED_VERSIONS[packagename] = version
