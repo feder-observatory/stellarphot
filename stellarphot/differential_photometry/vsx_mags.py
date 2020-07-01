dcc07ed7e437b5c17505c99d7c2e82906d4f1b2e
Matt Craig <mattwcraig@gmail.com>
2020-07-01 12:27:38 -0500

    Add a bit more documentation to aavso magnitude function

--- stellarphot/differential_photometry/vsx_mags.py
+++ stellarphot/differential_photometry/vsx_mags.py
@@ -62,16 +62,21 @@ def calc_vmag(var_stars, star_data, comp_stars):
     stdev : float
         Standard deviation for variable star values
     """
+
+    # Match variable stars (essentially a list of targets) to instrumental
+    # magnitude information.
     var_coords = var_stars['coords']
     star_data_coords = SkyCoord(ra=star_data['RA'], dec=star_data['Dec'])
     v_index, v_d2d, _ = var_coords.match_to_catalog_sky(star_data_coords)
+
     rcomps = comp_stars[comp_stars['band'] == 'Rc']
+
+    # Match comparison star list to instrumental magnitude information
     comp_coords = SkyCoord(ra=rcomps['ra'], dec=rcomps['dec'])
     index, d2d, _ = comp_coords.match_to_catalog_sky(star_data_coords)
     good = d2d < 1 * u.arcsec
-    good_d2d = d2d[good]
-    good_comp = comp_coords[good]
     good_index = index[good]
+
     vmag_image = star_data[v_index]['mag_inst_R']
     comp_star_mag = star_data[good_index]['mag_inst_R']
     a_index, a_d2d, _ = comp_coords.match_to_catalog_sky(comp_coords)
