dedebc43645b7026ec3f3b0a740db1639bb78a79
Matt Craig <mattwcraig@gmail.com>
2022-11-19 12:39:19 -0600

    Add a few properties and methods to FitsOpener
    These are intended to hide some of the logic for setting/obtaining values
    while providing an API to get the information.

--- stellarphot/visualization/fits_opener.py
+++ stellarphot/visualization/fits_opener.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+import warnings
 
 from astropy.io import fits
+from astropy.nddata import CCDData
 
 from ipyfilechooser import FileChooser
 
@@ -13,6 +15,7 @@ class FitsOpener:
 
         self._header = {}
         self._selected_cache = self._fc.selected
+        self.object = ""
         self.register_callback(lambda x: None)
 
     @property
@@ -28,6 +31,13 @@ class FitsOpener:
         return self._header
 
     @property
+    def ccd(self):
+        """
+        Return image as CCDData object
+        """
+        return CCDData.read(self.path)
+
+    @property
     def path(self):
         return Path(self._fc.selected)
 
@@ -35,10 +45,41 @@ class FitsOpener:
         if not self._header or self._fc.selected != self._selected_cache:
             self._selected_cache = self._fc.selected
             self._header = fits.getheader(self.path)
+        try:
+            self.object = self._header['object']
+        except KeyError:
+            pass
 
     def register_callback(self, callable):
+        """
+        Register a callback that is called when the value changes. This is
+        the alternative to observing a value, which is not implemented for
+        some reason. Only one callback function is allowed.
+
+        This wraps the user-supplied callable to also update the header.
+
+        Parameters
+        ----------
+
+        callable : function
+            A function that takes one argument.
+        """
         def wrap_call(change):
             self._set_header()
             callable(change)
 
         self._fc.register_callback(wrap_call)
+
+    def load_in_image_widget(self, image_widget):
+        """
+        Load the selected image into an `astrowidgets.ImageWidget`, doing the best
+        that can be done to suppress warnings.
+
+        Parameters
+        ----------
+
+        image_widget : `astrowidgets.ImageWidget`
+            The widget into which to load the image.
+        """
+        with warnings.catch_warnings():
+            image_widget.load_fits(str(self.path))
