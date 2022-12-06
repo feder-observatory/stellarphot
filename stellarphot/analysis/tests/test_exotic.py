b6c4e81e604b9964ad4caf5831e5b514025120e5
Matt Craig <mattwcraig@gmail.com>
2022-12-05 20:58:48 -0600

    Update exotic test to match new function signature

--- stellarphot/analysis/tests/test_exotic.py
+++ stellarphot/analysis/tests/test_exotic.py
@@ -3,5 +3,5 @@ from stellarphot.analysis.exotic import exotic_settings_widget, get_values_from_
 
 def test_json_has_stuff():
     widget = exotic_settings_widget()
-    stuff = get_values_from_widget('known', widget)
+    stuff = get_values_from_widget(widget, key='known')
     assert 'user_info' in stuff.keys()
