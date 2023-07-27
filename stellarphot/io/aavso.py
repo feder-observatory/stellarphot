efd24c044d3ea3e636215fd7e140f63323d67f40
Matt Craig <mattwcraig@gmail.com>
2023-07-27 13:00:03 -0500

    Nearly working version of writer

--- stellarphot/io/aavso.py
+++ stellarphot/io/aavso.py
@@ -1,11 +1,10 @@
 # Class to representa file in AAVSO extended format
 from dataclasses import dataclass, field
-from typing import Union
 from pathlib import Path
 from importlib import resources
 import yaml
 
-from astropy.table import Table
+from astropy.table import Table, Column
 
 __all__ = [
     "AAVSOExtendedFileFormat"
@@ -14,16 +13,28 @@ __all__ = [
 
 @dataclass
 class AAVSOExtendedFileFormat:
-    observer_code: str
+    obscode : str
     _type: str = 'EXTENDED'
     _software: str = 'stellarphot'
     delim: str = ","
-    date: str = 'HJD'
+    date_format: str = 'JD'
+    date: Column = field(default_factory=Column)
     obstype: str = 'CCD'
-    variable_data: Table = field(default_factory=Table)
-    check_data: Table = field(default_factory=Table)
-    comparison_data: Union[Table, None] = None
+    magnitude: Column = field(default_factory=Column)
+    magerr: Column = field(default_factory=Column)
+    filter: Column = field(default_factory=Column)
+    kmag: Column = field(default_factory=Column)
+    cmag: Column = field(default_factory=Column)
     ensemble: bool = False
+    group : int  = ""
+    trans : str = "NO"
+    chart : str = ""
+    notes : str = ""
+    starid : str = ""
+    cname : str = ""
+    kname : str = ""
+    airmass : Column = field(default_factory=Column)
+    mtype : str = "STD"
 
     @property
     def type(self):
@@ -32,6 +43,22 @@ class AAVSOExtendedFileFormat:
     @property
     def software(self):
         return self._software
+
+    def set_data_column(self, data, column_map, star_id=None):
+        if star_id is not None:
+            use_data = data[data['id'] == star_id]
+        else:
+            use_data = data
+        for source_column, destination_column in column_map.items():
+            try:
+                setattr(self, destination_column, use_data[source_column])
+            except AttributeError:
+                raise AttributeError(f"Column {destination_column} not allowed in AAVSO extended format")
+            except KeyError:
+                raise KeyError(f"Column {source_column} not found in data")
+
+    def set_column_from_single_value(self, value, column):
+        setattr(self, column, Column([value] * len(self.variable_mag), name=column))
 
     def set_data(self, destination, data,
                  time='time', mag='mag', error='err', airmass='airmass'):
@@ -47,14 +74,17 @@ class AAVSOExtendedFileFormat:
             error: data[error],
         }
         standard_data = Table(standard_data)
+
         if destination == "variable":
             self.variable_data = standard_data
         elif destination == "check":
             self.check_data = standard_data
+            del self.check_data[airmass]
         elif destination == "comparison":
             if self.ensemble:
                 raise ValueError("Cannot set comparison data for ensemble magnitudeas")
             self.comparison_data = standard_data
+            del self.comparison_data[airmass]
 
     def write(self, file):
         p = Path(file)
@@ -62,4 +92,34 @@ class AAVSOExtendedFileFormat:
         # Make a table
         table_description = resources.read_text('stellarphot.io', 'aavso_submission_schema.yml')
         table_structure = yaml.safe_load(table_description)
-        print(table_structure['data'].keys())
+        for key in table_structure['comments'].keys():
+            print(f"{key}: {hasattr(self, key.lower())}")
+        table_dict = {}
+        length = 0
+        for key in table_structure['data'].keys():
+            print(f"{key}: {hasattr(self, key.lower())}")
+            if isinstance((item := getattr(self, key.lower())), Column):
+                if len(item) == 0:
+                    item = "na"
+                table_dict[key] = item
+                length = len(item) if len(item) > length else length
+            else:
+                table_dict[key] = item
+        print(length)
+        for k, v in table_dict.items():
+            if len(v) != length:
+                print(f"{k=} {v=} {len(v)=}")
+
+                table_dict[k] = Column([v] * length, name=k)
+
+        table = Table(table_dict)
+        table.meta['comments'] = []
+        for key in table_structure['comments'].keys():
+            if key == "DATE":
+                value = self.date_format
+            else:
+                value = getattr(self, key.lower())
+            table.meta['comments'].append(f"{key} = {value}")
+
+        print(table)
+        return table
