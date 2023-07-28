Fixed units for image area to be pixels as per convention.

stellarphot/core.py
@@ -319,15 +319,15 @@ class PhotometryData(BaseEnhancedTable):
     fwhm_y                u.pix
     width                 u.pix
     aperture              u.pix
-    aperture_area         u.pix * u.pix
+    aperture_area         u.pix
     annulus_inner         u.pix
     annulus_outer         u.pix
-    annulus_area          u.pix * u.pix
+    annulus_area          u.pix
     aperture_sum          consistent count units
     annulus_sum           consistent count units
-    sky_per_pix_avg       consistent count units (per pixel sqauared)
-    sky_per_pix_med       consistent count units (per pixel sqauared)
-    sky_per_pix_std       consistent count units (per pixel sqauared)
+    sky_per_pix_avg       consistent count units (per pixel)
+    sky_per_pix_med       consistent count units (per pixel)
+    sky_per_pix_std       consistent count units (per pixel)
     aperture_net_cnts     consistent count units
     noise_cnts            consistent count units
     noise_electrons       u.electron
@@ -364,10 +364,10 @@ class PhotometryData(BaseEnhancedTable):
         'fwhm_y' : u.pix,
         'width' : u.pix,
         'aperture' : u.pix,
-        'aperture_area' : u.pix * u.pix,
+        'aperture_area' : u.pix,
         'annulus_inner' : u.pix,
         'annulus_outer' : u.pix,
-        'annulus_area' : u.pix * u.pix,
+        'annulus_area' : u.pix,
         'aperture_sum' : None,
         'annulus_sum' : None,
         'sky_per_pix_avg' : None,
@@ -445,13 +445,13 @@ class PhotometryData(BaseEnhancedTable):
                                     f"{input_data[this_col].unit}).")
             for this_col in counts_per_pixel_sqr_columns:
                 if cnts_unit is None:
-                    persqrunit = u.pixel**-2
+                    perpixel = u.pixel**-1
                 else:
-                    persqrunit = cnts_unit * u.pixel**-2
-                if input_data[this_col].unit != persqrunit:
+                    perpixel = cnts_unit * u.pixel**-1
+                if input_data[this_col].unit != perpixel:
                     raise ValueError(f"input_data['{this_col}'] has inconsistent units "
                                     f"with input_data['{counts_columns[0]}'] (should "
-                                    f"be {persqrunit} but it's "
+                                    f"be {perpixel} but it's "
                                     f"{input_data[this_col].unit}).")
 
             # Compute additional columns (not done yet)
@@ -809,5 +809,7 @@ class SourceListData(BaseEnhancedTable):
     def drop_x_y(self):
         # drop image-based positionsfrom existing SourceListData structure
         self.meta['has_x_y'] = False
-        self['xcenter'] = Column(data=np.full(len(self), np.nan), name='ra', unit=u.deg)
-        self['ycenter'] = Column(data=np.full(len(self), np.nan), name='dec', unit=u.deg)
+        self['xcenter'] = Column(data=np.full(len(self), np.nan), name='ra',
+                                 unit=u.deg)
+        self['ycenter'] = Column(data=np.full(len(self), np.nan), name='dec', 
+                                 unit=u.deg)

stellarphot/photometry/photometry.py
@@ -315,10 +315,11 @@ def single_image_photometry(ccd_image, sourcelist, camera, observatory_location,
     photom['aperture_sum'].unit = ccd_image.unit
     photom['annulus_sum'].unit = ccd_image.unit
     photom['aperture'] = apers.r * u.pixel
-    photom['aperture_area'] = apers.area * u.pixel * u.pixel
     photom['annulus_inner'] = anuls.r_in * u.pixel
     photom['annulus_outer'] = anuls.r_out * u.pixel
-    photom['annulus_area'] = anuls.area  * u.pixel * u.pixel
+    # By convention, area is in units of pixels (not pixels squared) in a digital image
+    photom['aperture_area'] = apers.area * u.pixel
+    photom['annulus_area'] = anuls.area  * u.pixel
 
     if reject_background_outliers:
         print('single_image_photometry: computing clipped sky stats ...')
@@ -329,9 +330,9 @@ def single_image_photometry(ccd_image, sourcelist, camera, observatory_location,
             print('single_image_photometry: BAD ANNULUS, stats set to np.nan!')
             avg_sky_per_pix, med_sky_per_pix, std_sky_per_pix = \
                 np.nan, np.nan, np.nan
-        photom['sky_per_pix_avg'] = avg_sky_per_pix * u.pixel**-2
-        photom['sky_per_pix_med'] = med_sky_per_pix * u.pixel**-2
-        photom['sky_per_pix_std'] = std_sky_per_pix * u.pixel**-2
+        photom['sky_per_pix_avg'] = avg_sky_per_pix * u.pixel**-1
+        photom['sky_per_pix_med'] = med_sky_per_pix * u.pixel**-1
+        photom['sky_per_pix_std'] = std_sky_per_pix * u.pixel**-1
         print('                          ...DONE computing clipped sky stats')
     else: # Don't reject outliers (but why would you do this?)
         print("single_image_photometry: WARNING: Computing sky per pixel without "
@@ -343,8 +344,8 @@ def single_image_photometry(ccd_image, sourcelist, camera, observatory_location,
             med_pp.append(np.median(annulus_data))
             std_pp.append(np.std(annulus_data))
         photom['sky_per_pix_avg'] = photom['annulus_sum'] / photom['annulus_area']
-        photom['sky_per_pix_med'] = np.array(med_pp) * ccd_image.unit * u.pixel**-2
-        photom['sky_per_pix_std'] = np.array(std_pp) * ccd_image.unit * u.pixel**-2
+        photom['sky_per_pix_med'] = np.array(med_pp) * ccd_image.unit * u.pixel**-1
+        photom['sky_per_pix_std'] = np.array(std_pp) * ccd_image.unit * u.pixel**-1
 
     # Compute counts using clipped stats on sky per pixel
     photom['aperture_net_cnts'] = (photom['aperture_sum'].value -