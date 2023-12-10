0481addf08187fc1d965f225029242c6c7d3fab2
Matt Craig <mattwcraig@gmail.com>
2023-12-10 10:26:54 -0600

    Drop python version check
    This used a deprecated version class from distutils, and ought to be
    enforced these days by conda and/or pip anyway.

--- stellarphot/__init__.py
+++ stellarphot/__init__.py
@@ -6,26 +6,4 @@
 from ._astropy_init import *   # noqa
 # ----------------------------------------------------------------------------
 
-# Enforce Python version check during package import.
-# This is the same check as the one at the top of setup.py
-import sys
-from distutils.version import LooseVersion
-
-__minimum_python_version__ = "3.10"
-
-__all__ = []
-
-
-class UnsupportedPythonError(Exception):
-    """
-    Wraps an exception to raise if a user tries to use stellarphot
-    with an unsupported version of Python.
-    """
-    pass
-
-
-if LooseVersion(sys.version) < LooseVersion(__minimum_python_version__):
-    raise UnsupportedPythonError("stellarphot does not support Python < {}"
-                                 .format(__minimum_python_version__))
-
 from .core import *
