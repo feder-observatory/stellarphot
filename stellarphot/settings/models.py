d08298adfaae5b1778186f0c0a217fe04d1e785f
Tanner Weyer <tweyer28@hotmail.com>
2023-12-26 09:50:34 -0600

    Add Exoplanet model
    (cherry picked from commit d9736f3dab893b40f177973eadb2c7cbac30f1db)
    (cherry picked from commit 3d7617e6a746e2d97e0000bbd6557e5b44008c8e)

--- stellarphot/settings/models.py
+++ stellarphot/settings/models.py
@@ -6,7 +6,15 @@ from pydantic import BaseModel, Field, conint
 
 from .autowidgets import CustomBoundedIntTex
 
-__all__ = ["ApertureSettings", "PhotometryFileSettings"]
+from astropy.time import Time
+from astropy.coordinates import SkyCoord
+from astropy.units import Quantity
+import astropy.units as u
+
+from ..core import QuantityType
+
+
+__all__ = ["ApertureSettings", "PhotometryFileSettings", "Exoplanet"]
 
 
 class ApertureSettings(BaseModel):
@@ -80,3 +88,97 @@ class PhotometryFileSettings(BaseModel):
     aperture_locations_file: Path = Field(
         filter_pattern=["*.ecsv", "*.csv"], default=""
     )
+
+
+class TimeType(Time):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        return Time(v)
+
+
+class SkyCoordType(SkyCoord):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        return SkyCoord(v)
+
+
+class Exoplanet(BaseModel):
+    """
+    Create an object representing an Exoplanet.
+
+    Parameters
+    ----------
+
+    epoch : TimeType
+        Epoch of the exoplanet as an astropy Time object, not required.
+
+    period : QuantityType
+        Period of the exoplanet as a Quantity with units of time, not required.
+
+    Identifier : Str
+        Identifier of the exoplanet as a string, required.
+
+    coordinate : SkyCoordType
+        Coordinates of the exoplanet as an astropy SkyCoord object, required. 
+    
+    depth : float
+        Depth of the exoplanet object as a float, not required.
+
+    duration : QuantityType
+        Duration of the exoplanet transit as a Quantity with units of time, not required. 
+
+    Examples
+    --------
+
+    To create an `Exoplanet` object, you can pass in the epoch, period, Identifier, coordinate, depth,
+    and duration as keyword arguments:
+
+    >>> planet  = Exoplanet(
+        epoch=Time(0, format="jd"),
+        period=0 * u.min,
+        identifier="",
+        coordinate=SkyCoord(ra="00:00:00.00", dec="+00:00:00.0", frame="icrs", unit=("hour", "degree")),
+        depth=0,
+        duration=0 * u.min,)
+    """
+    
+    epoch: TimeType | None = None
+    period: QuantityType | None = None
+    identifier: str
+    coordinate: SkyCoordType
+    depth: float | None = None
+    duration: QuantityType | None = None
+
+    class Config:
+        validate_all = True
+        validate_assignment = True
+        extra = "forbid"
+        json_encoders = {
+            Quantity: lambda v: f"{v.value} {v.unit}",
+            QuantityType: lambda v: f"{v.value} {v.unit}",
+            Time: lambda v: f"{v.value}",
+        }
+
+    @classmethod
+    def validate_period(cls, values):
+        if u.get_physical_type(values["period"]) != "time":
+            raise ValueError(
+                f"Period does not have time units,currently has "
+                f"{values['period'].unit} units."
+            )
+
+    @classmethod
+    def validate_duration(cls, values):
+        if u.get_physical_type(values["duration"]) != "time":
+            raise ValueError(
+                f"Period does not have time units,currently has "
+                f"{values['duration'].unit} units."
+            )
--- stellarphot/settings/tests/test_models.py
+++ stellarphot/settings/tests/test_models.py
@@ -1,8 +1,12 @@
+from astropy.time import Time
 from pydantic import ValidationError
 import pytest
 
 from stellarphot.settings.models import ApertureSettings
+from stellarphot.settings.models import Exoplanet
 
+from astropy.coordinates import SkyCoord
+import astropy.units as u
 
 DEFAULT_APERTURE_SETTINGS = dict(radius=5, gap=10, annulus_width=15)
 
@@ -29,3 +33,25 @@ def test_create_invalid_values(bad_one):
     bad_settings[bad_one] = -1
     with pytest.raises(ValidationError, match=bad_one):
         ApertureSettings(**bad_settings)
+
+
+DEFAULT_EXOPLANET_SETTINGS = dict(
+    epoch=Time(0, format="jd"),
+    period=0 * u.min,
+    identifier="",
+    coordinate=SkyCoord(
+        ra="00:00:00.00", dec="+00:00:00.0", frame="icrs", unit=("hour", "degree")
+    ),
+    depth=0,
+    duration=0 * u.min,
+)
+
+
+def test_create_exoplanet_correctly():
+    planet = Exoplanet(**DEFAULT_EXOPLANET_SETTINGS)
+    assert planet.epoch == DEFAULT_EXOPLANET_SETTINGS["epoch"]
+    assert planet.period == DEFAULT_EXOPLANET_SETTINGS["period"]
+    assert planet.identifier == DEFAULT_EXOPLANET_SETTINGS["identifier"]
+    assert planet.coordinate == DEFAULT_EXOPLANET_SETTINGS["coordinate"]
+    assert planet.depth == DEFAULT_EXOPLANET_SETTINGS["depth"]
+    assert planet.duration == DEFAULT_EXOPLANET_SETTINGS["duration"]
