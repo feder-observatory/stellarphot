cff05965754903de7c535e7dbc95ddf3dce19f10
Matt Craig <mattwcraig@gmail.com>
2024-02-21 15:06:37 -0600

    Add some default settings to auto-generated ui

--- stellarphot/settings/views.py
+++ stellarphot/settings/views.py
@@ -1,10 +1,21 @@
 from ipyautoui import AutoUi
 
+from .models import _extract_short_description
+
 __all__ = ["ui_generator"]
 
 
 def ui_generator(model):
     """
-    Silly function to generate an AutoUi object from a model.
+    Generate a user interface with ipyautoui with a few default settings.
+
+    Parameters
+    ----------
+    model : `pydantic.BaseModel` subclass
+        The model to generate the user interface for.
     """
-    return AutoUi(model)
+    ui = AutoUi(model)
+    ui.show_validation = False
+    ui.show_null = True
+    ui.description = _extract_short_description(model.__doc__)
+    return ui
