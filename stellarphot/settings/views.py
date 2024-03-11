a5282afb893abfd891336abe1e57ad7e865dc40b
Matt Craig <mattwcraig@gmail.com>
2024-03-11 13:20:01 -0500

    Fix unusable widgets by disabling coninuous_update

--- stellarphot/settings/views.py
+++ stellarphot/settings/views.py
@@ -15,7 +15,30 @@ def ui_generator(model):
         The model to generate the user interface for.
     """
     ui = AutoUi(model)
+
+    # validation is really messy looking right now, so suppress display of
+    # the validation errors. A green check or red x will still be displayed.
     ui.show_validation = False
+
+    # By default nullable are not shown at all but it seems much easier for
+    # the user to understand if they are shown but disabled.
     ui.show_null = True
+
+    # Set the description to the first sentence of the docstring. The default is
+    # to use the entire docstring, which is often too long.
     ui.description = _extract_short_description(model.__doc__)
+
+    # Validation is checked every time a value is changed, and the contents of each
+    # field are written to the widget if validation passes. In at least one case,
+    # the "Observatory" model, this is not helpful because the format of lat/lon is
+    # not necessarily what the user entered. Furthermore, you can't edit the value
+    # in the widget because it is overwritten every time validation passes. So, for
+    # now, we will disable this feature by turning continuous update off for most
+    # fields.
+    for widget in ui.di_widgets.values():
+        try:
+            widget.continuous_update = False
+        except AttributeError:
+            pass
+
     return ui
