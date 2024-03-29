d918e93f4af663ae25fccb0b8653ee9841e35837
Matt Craig <mattwcraig@gmail.com>
2024-03-29 12:24:22 -0500

    Add several methods to settings file saving

--- stellarphot/settings/settings_files.py
+++ stellarphot/settings/settings_files.py
@@ -6,16 +6,17 @@ from pydantic import BaseModel
 
 from .models import Camera, Observatory, PassbandMap
 
+__all__ = ["SavedSettings", "SETTINGS_FILE_VERSION"]
+
 # We will have to version settings formats, I think. Hopefully this changes rarely
 # or never.
 SETTINGS_FILE_VERSION = "2"  # value chosen to match amjor version of stellarphot
 
 
 class SavedFileOperations:
-    @classmethod
-    def save(cls, path, model):
-        file_path = path / cls.file_name
-        json_data = model.model_dump_json(indent=4)
+    def save(self, path: Path):
+        file_path = path / self.file_name
+        json_data = self.model_dump_json(indent=4)
         with file_path.open("w") as f:
             f.write(json_data)
 
@@ -27,39 +28,160 @@ class SavedFileOperations:
         with file_path.open() as f:
             return cls.model_validate_json(f.read())
 
+    def delete(self, settings_path, confirm=False):
+        """
+        Delete the settings file for this class.
+
+        Parameters
+        ----------
+        confirm : bool, optional
+            If True, the file is deleted. If False, a ValueError is raised.
+        """
+        if confirm:
+            file_path = settings_path / self.file_name
+            file_path.unlink()
+        else:
+            raise ValueError("You must confirm deletion by passing confirm=True")
+
 
 class Cameras(SavedFileOperations, BaseModel):
     # Using the ClassVar annotation means this is treated as a class variable rather
     # than a pydantic field. We don't pydantic storing the name of the settings file in
     # the settings file itself.
     file_name: ClassVar[str] = "cameras.json"
+    "Name of the file where the cameras are saved."
+
     items: dict[str, Camera]
+    "Dictionary of cameras, keyed by camera name."
 
 
-class Observatories(BaseModel):
+class Observatories(SavedFileOperations, BaseModel):
     file_name: ClassVar[str] = "observatories.json"
+    "Name of the file where the observatories are saved."
     items: dict[str, Observatory]
+    "Dictionary of observatories, keyed by observatory name."
 
 
-class PassbandMaps(BaseModel):
+class PassbandMaps(SavedFileOperations, BaseModel):
     file_name: ClassVar[str] = "passband_maps.json"
+    "Name of the file where the passband maps are saved."
     items: dict[str, PassbandMap]
+    "Dictionary of passband maps, keyed by passband map name."
 
 
 class SavedSettings:
-    def __init__(self):
-        data_dir = PlatformDirs(
-            "stellarphot", version=SETTINGS_FILE_VERSION
-        ).user_data_dir
-        self.settings_path = Path(data_dir)
-        if not self.settings_path.exists():
-            self.settings_path.mkdir(parents=True)
+    """
+    Handle loading and saving of settings files from disk.
+    """
+
+    def __init__(self, _testing_path=None, _create_path=True):
+        """
+        Parameters
+        ----------
+
+        _testing_path : Path, optional
+            Path to use for testing purposes. If not provided, the default path is used.
+
+        _create_path : bool, optional
+            If True, the directory where settings files are stored is created if it does
+            not exist. If False, the directory is not created, which is useful for
+            testing.
+        """
+        if _testing_path is not None:
+            data_dir = _testing_path
+        else:
+            data_dir = PlatformDirs(
+                "stellarphot", version=SETTINGS_FILE_VERSION
+            ).user_data_dir
+        self._settings_path = Path(data_dir)
+        if _create_path:
+            if not self.settings_path.exists():
+                self.settings_path.mkdir(parents=True)
+
+    @property
+    def settings_path(self):
+        """
+        Path to the directory where settings files are stored.
+        """
+        return self._settings_path
 
     @property
     def cameras(self) -> Cameras:
-        self._cameras = Cameras.load_model(self.settings_path)
-        return self._cameras
+        """
+        Cameras stored in the settings.
+        """
+        return Cameras.load_model(self.settings_path)
 
-    def add_camera(self, camera: Camera):
-        self.cameras.items[camera.name] = camera
-        Cameras.save(self.settings_path, self._cameras)
+    @property
+    def observatories(self) -> Observatories:
+        """
+        Observatories stored in the settings.
+        """
+        return Observatories.load_model(self.settings_path)
+
+    @property
+    def passband_maps(self) -> PassbandMaps:
+        """
+        Passband maps stored in the settings.
+        """
+        return PassbandMaps.load_model(self.settings_path)
+
+    def get_items(self, item_type):
+        """
+        Get the items of a given type.
+
+        Parameters
+        ----------
+        item_type : str | Camera | Observatory | PassbandMap
+            The type of item to get.
+        """
+        match item_type:
+            case Camera() | "camera":
+                return self.cameras
+            case Observatory() | "observatory":
+                return self.observatories
+            case PassbandMap() | "passband_map":
+                return self.passband_maps
+            case _:
+                raise ValueError(f"Unknown item type {item_type}")
+
+    def add_item(self, item):
+        """
+        Add an item to the settings.
+
+        Parameters
+        ----------
+        item : Camera | Observatory | PassbandMap
+            The item to add.
+        """
+        match item:
+            case Camera() as to_add:
+                container = self.cameras
+            case Observatory() as to_add:
+                container = self.observatories
+            case PassbandMap() as to_add:
+                container = self.passband_maps
+            case _:
+                raise ValueError("Unknown item type")
+
+        if to_add.name in container.items:
+            raise ValueError(f"{to_add.name} already exists in {container.file_name}")
+
+        container.items[to_add.name] = to_add
+        container.save(self.settings_path)
+
+    def delete(self, confirm=False):
+        """
+        Delete all settings files.
+
+        Parameters
+        ----------
+        confirm : bool, optional
+            If True, the files are deleted. If False, a ValueError is raised.
+        """
+        if not confirm:
+            raise ValueError("You must confirm deletion by passing confirm=True")
+        self.cameras.delete(self.settings_path, confirm=confirm)
+        self.observatories.delete(self.settings_path, confirm=confirm)
+        self.passband_maps.delete(self.settings_path, confirm=confirm)
+        self.settings_path.rmdir()
---
+++ stellarphot/settings/tests/test_settings_file.py
@@ -0,0 +1,179 @@
+import pytest
+
+from stellarphot.settings import (
+    SETTINGS_FILE_VERSION,
+    Camera,
+    Observatory,
+    PassbandMap,
+    SavedSettings,
+)
+
+CAMERA = """
+{
+    "name": "Aspen CG 16m",
+    "data_unit": "adu",
+    "gain": "1.5 electron / adu",
+    "read_noise": "10.0 electron",
+    "dark_current": "0.01 electron / s",
+    "pixel_scale": "0.6 arcsec / pix",
+    "max_data_value": "50000.0 adu"
+}
+"""
+
+OBSERVATORY = """
+{
+    "name": "Feder",
+    "latitude": "46d52m25.68s",
+    "longitude": "263d13m55.92s",
+    "elevation": "311.0 m",
+    "AAVSO_code": null,
+    "TESS_telescope_code": null
+}
+"""
+
+PASSBAND_MAP = """
+{
+    "name": "Filter wheel 1",
+    "your_filter_names_to_aavso": [
+        {
+            "your_filter_name": "rp",
+            "aavso_filter_name": "SR"
+        },
+        {
+            "your_filter_name": "gp",
+            "aavso_filter_name": "SG"
+        }
+    ]
+}
+"""
+
+
+class TestSavedSettings:
+    def test_settings_path_contains_package_and_version(self):
+        saved_settings = SavedSettings(_create_path=False)
+        assert "stellarphot" in str(saved_settings.settings_path)
+        assert SETTINGS_FILE_VERSION in str(saved_settings.settings_path)
+
+    @pytest.mark.parametrize(
+        "klass,item_json",
+        [(Camera, CAMERA), (Observatory, OBSERVATORY), (PassbandMap, PASSBAND_MAP)],
+    )
+    def test_add_saved_item(self, klass, item_json, tmp_path):
+        # Test that items are properly saved and loaded.
+        saved_settings = SavedSettings(_testing_path=tmp_path)
+        assert saved_settings.settings_path.exists()
+        # Add a camera.
+        item = klass.model_validate_json(item_json)
+        saved_settings.add_item(item)
+        # Load the cameras
+        saved_items = saved_settings.get_items(item)
+        assert len(saved_items.items) == 1
+        assert saved_items.items[item.name] == item
+
+    @pytest.mark.parametrize(
+        "klass,item_json",
+        [(Camera, CAMERA), (Observatory, OBSERVATORY), (PassbandMap, PASSBAND_MAP)],
+    )
+    def test_adding_multiple_items_of_same_type(self, klass, item_json, tmp_path):
+        # Test that items are properly saved and loaded.
+        saved_settings = SavedSettings(_testing_path=tmp_path)
+        assert saved_settings.settings_path.exists()
+        # Add an item
+        item1 = klass.model_validate_json(item_json)
+        item2 = klass.model_validate_json(
+            item_json.replace(item1.name, item1.name + "2")
+        )
+        saved_settings.add_item(item1)
+        saved_settings.add_item(item2)
+        # Load the items -- any instance of the class (e.g. Camera) or a
+        # string (e.g. "camera") should work for getting the items.
+        saved_items = saved_settings.get_items(item1)
+        assert len(saved_items.items) == 2
+        assert saved_items.items[item2.name] == item2
+        assert saved_items.items[item1.name] == item1
+
+    def test_add_existing_saved_item_raises_error(self, tmp_path):
+        # Test that adding an existing camera raises an error. Other items follow the
+        # same pattern, so only cameras are tested.
+        saved_settings = SavedSettings(_testing_path=tmp_path)
+        # Add a camera.
+        item = Camera.model_validate_json(CAMERA)
+        saved_settings.add_item(item)
+        # Add the same camera again.
+        with pytest.raises(
+            ValueError, match="Aspen CG 16m already exists in cameras.json"
+        ):
+            saved_settings.add_item(item)
+
+    def test_adding_multiple_types_of_items(self, tmp_path):
+        # Test that adding multiple types of items works.
+        saved_settings = SavedSettings(_testing_path=tmp_path)
+        # Add a camera.
+        camera = Camera.model_validate_json(CAMERA)
+        saved_settings.add_item(camera)
+        # Add an observatory.
+        observatory = Observatory.model_validate_json(OBSERVATORY)
+        saved_settings.add_item(observatory)
+        # Add a passband map.
+        passband_map = PassbandMap.model_validate_json(PASSBAND_MAP)
+        saved_settings.add_item(passband_map)
+        # Load the items
+        cameras = saved_settings.get_items("camera")
+        assert len(cameras.items) == 1
+        assert cameras.items[camera.name] == camera
+        observatories = saved_settings.get_items("observatory")
+        assert len(observatories.items) == 1
+        assert observatories.items[observatory.name] == observatory
+        passband_maps = saved_settings.get_items("passband_map")
+        assert len(passband_maps.items) == 1
+        assert passband_maps.items[passband_map.name] == passband_map
+
+    def test_delete_without_confirm_raises_error(self):
+        # Trying to delete settings without confirming should raise an error.
+        saved_settings = SavedSettings(_create_path=False)
+        with pytest.raises(ValueError, match="You must confirm deletion by passing"):
+            saved_settings.cameras.delete(saved_settings.settings_path)
+
+    def test_delete_with_confirm_deletes_file(self, tmp_path):
+        # Test that deleting a settings file works.
+        saved_settings = SavedSettings(_testing_path=tmp_path)
+        # Add a camera.
+        camera = Camera.model_validate_json(CAMERA)
+        saved_settings.add_item(camera)
+        # Delete the camera.
+        saved_settings.cameras.delete(saved_settings.settings_path, confirm=True)
+        assert not (
+            saved_settings.settings_path / saved_settings.cameras.file_name
+        ).exists()
+
+    def test_deleting_all_settings_without_confirm_raises_error(self):
+        # Trying to delete all settings without confirming should raise an error.
+        saved_settings = SavedSettings(_create_path=False)
+        with pytest.raises(ValueError, match="You must confirm deletion by passing"):
+            saved_settings.delete()
+
+    def test_delete_all_settings_with_confirm_deletes_files(self, tmp_path):
+        # Test that deleting all settings files works.
+        saved_settings = SavedSettings(_testing_path=tmp_path)
+        # Add a camera.
+        camera = Camera.model_validate_json(CAMERA)
+        saved_settings.add_item(camera)
+        # Add an observatory.
+        observatory = Observatory.model_validate_json(OBSERVATORY)
+        saved_settings.add_item(observatory)
+        # Add a passband map.
+        passband_map = PassbandMap.model_validate_json(PASSBAND_MAP)
+        saved_settings.add_item(passband_map)
+
+        # Delete all settings.
+        saved_settings.delete(confirm=True)
+        assert not (
+            saved_settings.settings_path / saved_settings.cameras.file_name
+        ).exists()
+        assert not (
+            saved_settings.settings_path / saved_settings.observatories.file_name
+        ).exists()
+        assert not (
+            saved_settings.settings_path / saved_settings.passband_maps.file_name
+        ).exists()
+        assert not saved_settings.settings_path.exists()
