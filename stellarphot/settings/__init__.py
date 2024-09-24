b67959dc320e9dde27ca9f87a2129f61ebf5f642
Matt Craig <mattwcraig@gmail.com>
2024-09-24 17:11:47 -0500

    Add schema and example to settings namespace

--- stellarphot/settings/__init__.py
+++ stellarphot/settings/__init__.py
@@ -1,3 +1,15 @@
 from .models import *
 from .settings_files import *
 from .views import *
+
+# For conveniences, provide the JSON schema and an example of PhotometrySettings.
+photometry_settings_schema = PhotometrySettings.model_json_schema()  # noqa: F405
+
+from .tests.test_models import TEST_PHOTOMETRY_SETTINGS
+
+photometry_settings_example = PhotometrySettings(  # noqa: F405
+    **TEST_PHOTOMETRY_SETTINGS
+)
+
+# Clean up the namespace
+del TEST_PHOTOMETRY_SETTINGS
