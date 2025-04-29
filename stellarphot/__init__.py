e26f80aef8469d7832e8d9534379bd9e37df4a8a
Matt Craig <mattwcraig@gmail.com>
2025-04-29 11:27:22 -0500

    Put version back in

--- stellarphot/__init__.py
+++ stellarphot/__init__.py
@@ -3,6 +3,10 @@
 # Packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
+try:
+    from .version import version as __version__
+except ImportError:
+    __version__ = ""
 
 from .core import *
 
