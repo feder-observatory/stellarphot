a11d2cc0b7f723cdeb70905d4c12e9d865dc51ff
Matt Craig <mattwcraig@gmail.com>
2025-05-11 11:16:24 -0500

    Add structure for making transforms between magnitude systems

--- stellarphot/utils/magnitude_system_transforms.py
+++ stellarphot/utils/magnitude_system_transforms.py
@@ -1,7 +1,18 @@
 from enum import StrEnum
+from pathlib import Path
 from typing import Annotated
 
-from pydantic import BaseModel, Field
+import numpy as np
+from astropy.utils.data import get_pkg_data_path
+from pydantic import BaseModel, BeforeValidator, Field
+
+__all__ = [
+    "MagnitudeSystemNames",
+    "MagnitudeSystem",
+    "MagnitudeTransform",
+    "MagnitudeSystemTransform",
+    "PanStarrs1ToJohnsonCousins",
+]
 
 
 class MagnitudeSystemNames(StrEnum):
@@ -17,11 +28,58 @@ class MagnitudeSystemNames(StrEnum):
     UGRIZ = "ugriz"
 
 
-class MagnitudeSystemPassbands(BaseModel):
-    system_name: MagnitudeSystemNames
-    passbands_names: list[str]
-    passband_range: tuple[float, float]
-    # Down the road add something like symphot.SpectralElement
+# class MagnitudePassband(BaseModel):
+#     name: str
+#     # passband_range: tuple[float, float]
+#     # Down the road add something like symphot.SpectralElement
+
+
+class MagnitudeSystem(BaseModel):
+    """
+    Class for a magnitude system.
+    """
+
+    name: MagnitudeSystemNames
+    passbands: Annotated[
+        list[str],
+        Field(description=("List of passbands in the system.")),
+    ]
+
+
+class MagnitudeTransform(BaseModel):
+    """
+    Class for magnitude transformation from one set of passbands to another.
+    """
+
+    name: str
+    from_passband: Annotated[
+        str,
+        Field(description="Passband to transform from."),
+    ]
+    to_passband: Annotated[
+        str,
+        Field(description="Passband to transform to."),
+    ]
+    polynomial_coefficients: Annotated[
+        list[float],
+        Field(
+            description=(
+                "Coefficients for the transformation. The "
+                "number of coefficients depends on the transformation."
+            )
+        ),
+    ]
+    residual: Annotated[
+        float,
+        Field(description=("Residual of the transformation.")),
+    ]
+
+    @property
+    def polynomial(self) -> np.polynomial.Polynomial:
+        """
+        Polynomial transformation.
+        """
+        return np.polynomial.Polynomial(self.polynomial_coefficients)
 
 
 class MagnitudeSystemTransform(BaseModel):
@@ -39,18 +97,104 @@ class MagnitudeSystemTransform(BaseModel):
             )
         ),
     ]
-    from_system: MagnitudeSystemNames = Field(
+    from_system: MagnitudeSystem = Field(
         description="The magnitude system to transform from."
     )
-    to_system: MagnitudeSystemNames = Field(
+    to_system: MagnitudeSystem = Field(
         description="The magnitude system to transform to."
     )
-    transform_coeofficients: Annotated[
-        list[float],
+    transform_coefficients: Annotated[
+        dict[tuple[str, str], MagnitudeTransform],
         Field(
             description=(
                 "Coefficients for the transformation. The "
                 "number of coefficients depends on the transformation."
             )
         ),
+        BeforeValidator(
+            lambda x: {
+                tuple(k.split(",")) if isinstance(k, str) else k: v
+                for k, v in x.items()
+            }
+        ),
     ]
+
+
+class PanStarrs1ToJohnsonCousinsMixin:
+    """
+    Implementation of the Pan-STARRS1 to Johnson-Cousins transformation.
+    """
+
+    def __call__(
+        self, from_magnitudes: np.typing.ArrayLike, ps1_band_for_V: str = "gp1"
+    ) -> np.ndarray:
+        """
+        Transform Pan-STARRS1 magnitudes to Johnson-Cousins magnitudes.
+
+        Parameters
+        ----------
+        from_magnitudes : np.ArrayLike
+            Pan-STARRS1 magnitudes to transform. The shape should either be (6,)
+            or (n, 6), where n is the number of magnitudesin the Pan-STARRS1 system
+            you wish to transform to Johnson-Cousins system.
+
+        """
+        # Convert to numpy array
+        from_magnitudes = np.asarray(from_magnitudes)
+
+        # The Pan-STARRS1 transformation is a polynomial of order 2
+        # in which the independent variable, which they call "x",
+        # is g_p1 - r_p1.
+        from_band_index = {
+            band: index for index, band in enumerate(self.from_system.passbands)
+        }
+        color = from_magnitudes[..., 0] - from_magnitudes[..., 1]
+        if len(from_magnitudes.shape) == 1:
+            from_magnitudes = from_magnitudes[np.newaxis, :]
+        to_mags = np.zeros(
+            from_magnitudes.shape[:-1] + (len(self.to_system.passbands),)
+        )
+        for index, jc_band in enumerate(self.to_system.passbands):
+            to_band_name = jc_band
+            match to_band_name:
+                case "B":
+                    from_band_name = "gp1"
+
+                case "V":
+                    # THere are three options in the Pan-STARRS1 paper
+                    # for the V band: g_p1, r_p1, and w_p1.
+                    from_band_name = ps1_band_for_V
+                case "Rc":
+                    from_band_name = "rp1"
+                case "Ic":
+                    from_band_name = "ip1"
+                case _:  # pragma: no cover
+                    raise ValueError(
+                        f"Unknown band name {to_band_name} for "
+                        f"transformation from Pan-STARRS1 to Johnson-Cousins."
+                    )
+
+            to_mags[..., index] = (
+                self.transform_coefficients[(from_band_name, to_band_name)].polynomial(
+                    color
+                )
+                + from_magnitudes[..., from_band_index[from_band_name]]
+            )
+        return np.squeeze(to_mags)
+
+
+class PanStarrs1ToJohnsonCousins(
+    PanStarrs1ToJohnsonCousinsMixin, MagnitudeSystemTransform
+):
+    """
+    Class for transforming Pan-STARRS1 magnitudes to Johnson-Cousins magnitudes.
+    """
+
+    @classmethod
+    def load(cls) -> "PanStarrs1ToJohnsonCousins":
+        """
+        Load the Pan-STARRS1 to Johnson-Cousins transformation from a file.
+        """
+        # Load the transformation from a file
+        path = Path(get_pkg_data_path("data/PS1_to_JC.json"))
+        return cls.model_validate_json(path.read_text())
---
+++ stellarphot/utils/tests/test_magnitude_system_transforms.py
@@ -0,0 +1,174 @@
+# The goal here is to both test code and the data files
+from pathlib import Path
+
+import numpy as np
+import pytest
+from astropy.utils.data import get_pkg_data_path
+from pydantic import ValidationError
+
+from stellarphot.utils.magnitude_system_transforms import (
+    MagnitudeSystem,
+    MagnitudeSystemNames,
+    MagnitudeSystemTransform,
+    MagnitudeTransform,
+    PanStarrs1ToJohnsonCousins,
+)
+
+
+def _check_json_roundtrip(model):
+    """
+    Check that the model can be serialized to JSON and then deserialized
+    back to the same model.
+    """
+    json_str = model.model_dump_json()
+    new_model = model.model_validate_json(json_str)
+    assert model == new_model
+
+
+class TestMagnitudeSystem:
+    def test_can_create_and_jc_file_is_good(self):
+        jc_bands = ["U", "B", "V", "Rc", "Ic"]
+        sys = MagnitudeSystem(name=MagnitudeSystemNames.JC, passbands=jc_bands)
+        assert sys.name == MagnitudeSystemNames.JC
+        assert sys.passbands == jc_bands
+
+        # Check that Johnson-Cousins that we ship is as expected
+        from_data = Path(
+            get_pkg_data_path("data/JohnsonCousins.json", package="stellarphot.utils")
+        )
+        assert from_data.exists()
+        assert sys == MagnitudeSystem.model_validate_json(from_data.read_text())
+
+        # Check serialization roundtrip
+        _check_json_roundtrip(sys)
+
+    def test_no_unknown_systems(self):
+        with pytest.raises(ValidationError):
+            MagnitudeSystem(name="Unknown", passbands=["U", "B", "V", "R", "I"])
+
+    def test_panstarrs1_system_def(self):
+        ps1_file = Path(
+            get_pkg_data_path("data/pan-starrs1.json", package="stellarphot.utils")
+        )
+        ps1 = MagnitudeSystem.model_validate_json(ps1_file.read_text())
+        assert ps1.name == MagnitudeSystemNames.PANSTARRS1
+        assert ps1.passbands == ["gp1", "rp1", "ip1", "zp1", "yp1", "wp1"]
+
+
+class TestMagnitudeTransform:
+    def test_make_transform(self):
+        fake_coeff = [0.1, 0.2, 0.3]
+        # This is NOT a real transform....just making sure that
+        # the transform can be made
+        my_transform = MagnitudeTransform(
+            name="test",
+            from_passband="B",
+            to_passband="gp1",
+            polynomial_coefficients=fake_coeff,
+            residual=0.1,
+        )
+
+        assert my_transform.name == "test"
+        assert np.polynomial.Polynomial(fake_coeff) == my_transform.polynomial
+        val = 2
+        assert np.isclose(
+            my_transform.polynomial(val), (0.1 + 0.2 * val + 0.3 * val**2)
+        )
+
+        # check roundtrip serialization
+        _check_json_roundtrip(my_transform)
+
+
+class TestMagnitudeSystemTransform:
+    def test_make_transform(self):
+        my_jc_system = MagnitudeSystem(
+            name=MagnitudeSystemNames.JC, passbands=["B", "V"]
+        )
+        my_ps1_system = MagnitudeSystem(
+            name=MagnitudeSystemNames.PANSTARRS1, passbands=["gp1", "rp1"]
+        )
+
+        fake_coeff = [0.1, 0.2, 0.3]
+        my_mag_trans1 = MagnitudeTransform(
+            name="test",
+            from_passband="B",
+            to_passband="gp1",
+            polynomial_coefficients=fake_coeff,
+            residual=0.1,
+        )
+
+        my_mag_trans2 = MagnitudeTransform(
+            name="test",
+            from_passband="V",
+            to_passband="rp1",
+            polynomial_coefficients=fake_coeff,
+            residual=0.1,
+        )
+
+        my_transform = MagnitudeSystemTransform(
+            name="test",
+            reference="some paper I will never write",
+            from_system=my_jc_system,
+            to_system=my_ps1_system,
+            transform_coefficients={
+                ("B", "gp1"): my_mag_trans1,
+                ("V", "rp1"): my_mag_trans2,
+            },
+        )
+
+        # Make sure we can access the transform coefficients as intended.
+        assert my_transform.transform_coefficients[("B", "gp1")] == my_mag_trans1
+        assert my_transform.transform_coefficients[("V", "rp1")] == my_mag_trans2
+
+        # Serialize the model and make sure that the transform keys appear
+        # as expected.
+        serialized = my_transform.model_dump_json()
+        assert "B,gp1" in serialized
+        assert "V,rp1" in serialized
+
+        # Check that the transform can roundtrip to json
+        _check_json_roundtrip(my_transform)
+
+
+class TestPanStarrs1ToJohnsonCousins:
+    @pytest.mark.parametrize("setup_method", ["class", "load"])
+    def test_transform(self, setup_method):
+        if setup_method == "class":
+            # Here we just read in the transform on disk rather than doing it manually.
+            ps1_file = Path(
+                get_pkg_data_path("data/PS1_to_JC.json", package="stellarphot.utils")
+            )
+            ps1_to_jc = PanStarrs1ToJohnsonCousins.model_validate_json(
+                ps1_file.read_text()
+            )
+        else:
+            ps1_to_jc = PanStarrs1ToJohnsonCousins.load()
+
+        # Spot check a couple of transform relationships
+        # Expected polynomials from the paper
+        gp1_B_poly = np.polynomial.Polynomial([0.212, 0.556, 0.034])
+        assert ps1_to_jc.transform_coefficients[("gp1", "B")].polynomial == gp1_B_poly
+
+        # Check that the transform works
+        fake_gp1_mags = np.array(
+            [
+                [20.0, 19.0, 18.0, 17.0, 16.0, 15.0],
+                [20.0, 19.0, 18.0, 17.0, 16.0, 15.0],
+                [20.0, 19.0, 18.0, 17.0, 16.0, 15.0],
+            ]
+        )
+        # Trensform just a single row
+        fake_jc_mags = ps1_to_jc(fake_gp1_mags[1, :])
+        assert fake_jc_mags.shape == (4,)
+        assert np.isclose(
+            fake_jc_mags[0],
+            gp1_B_poly(fake_gp1_mags[0, 0] - fake_gp1_mags[0, 1]) + fake_gp1_mags[0, 0],
+        )
+
+        # Transform it all
+        fake_jc_mags = ps1_to_jc(fake_gp1_mags)
+        assert fake_jc_mags.shape == (3, 4)
+        assert np.isclose(
+            fake_jc_mags[0, 0],
+            gp1_B_poly(fake_gp1_mags[0, 0] - fake_gp1_mags[0, 1]) + fake_gp1_mags[0, 0],
+        )
